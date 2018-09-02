328080c9e88458cfd46d000c029c22ae725151b8
Scott Kuhl <scott@kuhl.ws>
2018-09-02 12:11:33 -0500

    Initial project.

--- Sherlock/Program.cs
+++ Sherlock/Program.cs
@@ -2,11 +2,36 @@
 
 namespace Sherlock
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string press = { "a reporter from the Times", "a reporter from the Observer" };
+            string family = {"Mycroft Holmes", "Mrs. Hudson" };
+            string enemies = {"Professor Moriarty", "Charles "Augustus" Milverton", "John Woodley" };
+            string loveInterests = {"Irene Adler" };
+            string friends = {"Inspector Lestrade", "Dr. Watson" };
+
+            Console.WriteLine(KnockKnock(press));
+            Console.WriteLine(KnockKnock(family));
+            Console.WriteLine(KnockKnock(enemies));
+            Console.WriteLine(KnockKnock(loveInterests));
+            Console.WriteLine(KnockKnock(friends));
+        }
+
+        private static string KnockKnock(string[] guests)
+        {
+            foreach (string guest in guests)
+            {
+                if (guest == "Dr Watson" || guest == "Inspector Lestrade")
+                {
+                    return "Enter";
+                }
+                else
+                {
+                    return "Go Away! (sound of violin music...)";
+                }
+            }
         }
     }
 }
